57fae59fe1c83aff77e79c91e3a1d7d01acbe62a
Eathyn <eathynzakas@gmail.com>
2021-04-11 15:56:15 +0800

    feat: add Docker component

---
+++ src/views/home/Docker.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="docker">
+    <div
+      v-for="(item, index) in dockerList"
+      :key="item.icon"
+      class="docker__item"
+      :class="{ 'docker__item--active':  index === 0 }"
+    >
+      <div class="iconfont" v-html="item.icon"></div>
+      <div class="docker__title">{{ item.text }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Docker',
+  setup () {
+    const dockerList = [
+      { icon: '&#xe9ce;', text: '首页' },
+      { icon: '&#xe603;', text: '购物车' },
+      { icon: '&#xe63e;', text: '订单' },
+      { icon: '&#xe660;', text: '我的' }
+    ]
+    return { dockerList }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../style/variables.scss';
+@import '../../style/mixins.scss';
+
+.docker {
+  display: flex;
+  box-sizing: border-box;
+  position: absolute;
+  padding: 0 .18rem;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: .49rem;
+  border-top: .01rem solid $content-bgColor;
+  color: $content-fontColor;
+  &__item {
+    flex: 1;
+    text-align: center;
+    .iconfont {
+      margin: .07rem 0 .02rem 0;
+      font-size: .18rem;
+    }
+    &--active {
+      color: #1FA4FC;
+    }
+  }
+  &__title {
+    font-size: .2rem;
+    transform: scale(.5, .5);
+    transform-origin: center top;
+  }
+}
+</style>
