c821f43aeb62fe558c74ef2f31a041017d6b44f3
Eathyn <eathynzakas@gmail.com>
2021-04-11 17:16:02 +0800

    feat: add / and /login path

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,19 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
+import Login from '../views/login/Login'
+import Home from '../views/home/Home'
 
-const routes = []
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  }
+]
 
 const router = createRouter({
   history: createWebHashHistory(),
